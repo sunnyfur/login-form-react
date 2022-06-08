308cf0f9dcb5b19585bf86becedd302da93ad87f
sunnyfur <polin.makarova@mail.ru>
2022-06-08 13:18:23 +0500

    fix:  material ui form

--- src/Components/SignForm.jsx
+++ src/Components/SignForm.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import { Button, TextField, Grid, Paper } from "@mui/material";
+
+import "./SignForm.css";
 
 const validationSchema = yup.object({
   email: yup
@@ -25,27 +26,43 @@ const SignUpForm = () => {
   });
 
   return (
-    <div>
+    <Paper elevation={24} className="form">
       <h1>Signup</h1>
       <form onSubmit={formik.handleSubmit}>
-        <TextField
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.toucched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          error={formik.toucched.password && Boolean(formik.errors.password)}
-          type="password"
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              id="email"
+              name="email"
+              label="email"
+              fullWidth
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="password"
+              name="password"
+              label="password"
+              fullWidth
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              type="password"
+              helperText={formik.touched.password && formik.errors.password}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
       </form>
-    </div>
+    </Paper>
   );
 };
 export default SignUpForm;
